a03e1db82250944155e17dea18cfdaa0e9ef0d0e
Corey Barr <corey.jbarr@gmail.com>
2020-02-05 13:37:00 -0600

    js jquerey sourced and running on server

--- server/server.js
+++ server/server.js
@@ -6,9 +6,21 @@ const bodyParser = require ('body-parser');
 const app = express();
 const port = 5000;
 
+let quotes = [
+    {who:"Denzel", quote:"ease larger something"},
+    {who:"MSCott", quote:"shots you don't take"},
+    {who:"Corey", quote:"woof woof"},
+    {who:"Dev", quote:"fortune cookie BS"}
+];
+
 //uses
-app.use (express.static('server/public'));
+app.use(express.static('server/public'));
 //spin up server
 app.listen(port,(req,res)=>{
     console.log("The server is up on", port)
 })//end spin up server
+
+app.get( '/quotes', (req, res)=>{
+    console.log("quotes GET");
+    res.send(quotes)
+})
